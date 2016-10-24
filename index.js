bea843b546ae2357393398ccc7a773da96b8675d
Kegan Dougal <kegsay@gmail.com>
2016-10-24 23:37:56 +0100

    Parse out the typescript definition AST, using typescript no less

--- index.js
+++ index.js
@@ -1,3 +1,21 @@
-module.exports = function(input) {
+var typescript = require("typescript");
+
+var debugReverseMappings = Object.create(null);
+
+function walkAst(node) {
+    console.log(debugReverseMappings[node.kind]);
+    typescript.forEachChild(node, walkAst);
+}
+
+
+module.exports = function(input, opts) {
+    // Use typescript itself to extract the AST
+    var sourceFile = typescript.createSourceFile("tmp.d.ts", input, typescript.ScriptTarget.ES6, true);
+    Object.keys(typescript.SyntaxKind).forEach(function(k) {
+        debugReverseMappings[typescript.SyntaxKind[k]] = k;
+    });
+
+    walkAst(sourceFile);
+
     return input;
 }
