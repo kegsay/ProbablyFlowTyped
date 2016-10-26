5160964a0dc7f590642d4b066f16ef813efda1fe
Kegan Dougal <kegsay@gmail.com>
2016-10-26 23:29:47 +0100

    Add deep-diff failing tests

--- tests/runner.js
+++ tests/runner.js
@@ -6,8 +6,15 @@ var probsTyped = require("../index");
 
 var defsDir = path.join(__dirname, "/defs");
 
+var BOLD_ON = "\x1b[1m";
+var BOLD_OFF = "\x1b[22m";
 var RED = "\x1b[41m";
-var RESET = "\x1b[49m";
+var FG_RED = "\x1b[31m";
+var FG_GREEN = "\x1b[32m";
+var RESET = "\x1b[0m";
+
+var OKAY = BOLD_ON + FG_GREEN + "  OK" + RESET + "  ";
+var FAIL = BOLD_ON + FG_RED + "FAIL" + RESET + "  ";
 
 glob(defsDir + "/*.d.ts", function(err, files) {
     if (err) {
@@ -36,10 +43,12 @@ glob(defsDir + "/*.d.ts", function(err, files) {
         });
 
         if (actualOutLines.length === expectedOutLines.length) {
+            var isOkay = true;
             for (var lineNum = 0; lineNum < actualOutLines.length; lineNum++) {
                 if (actualOutLines[lineNum] === expectedOutLines[lineNum]) {
                     continue;
                 }
+                isOkay = false;
                 var lineLength = Math.max(actualOutLines[lineNum].length, expectedOutLines[lineNum].length);
                 for (var charNum = 0; charNum < lineLength; charNum++) {
                     if (actualOutLines[lineNum][charNum] === expectedOutLines[lineNum][charNum]) {
@@ -50,7 +59,7 @@ glob(defsDir + "/*.d.ts", function(err, files) {
                     var expectedSnippet = snippet(expectedOutLines[lineNum], charNum);
                     
                     console.error(
-                        "%s:%s:%s %s", outFile, lineNum + 1, charNum + 1,
+                        "%s%s:%s:%s %s", FAIL, outFile, lineNum + 1, charNum + 1,
                         "Unexpected char: '" + badChar + "'. " +
                         "Got \"" + actualSnippet + "\", expected \"" + expectedSnippet + "\"."
                     );
@@ -58,10 +67,13 @@ glob(defsDir + "/*.d.ts", function(err, files) {
                     break;
                 }
             }
+            if (isOkay) {
+                console.log("%s%s", OKAY, outFile);
+            }
         }
         else {
             console.error(
-                "%s:%s %s", outFile, 0,
+                "%s%s:%s %s", FAIL, outFile, 0,
                 "Expected " + expectedOutLines.length + " lines, got " + actualOutLines.length
             );
             numFails += 1;
